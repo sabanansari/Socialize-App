5a3f42a762d3af8e2270737d3b295afe0d582423
sabanansari <alisaban52@gmail.com>
2020-07-13 13:17:44 +0530

    search page UI updated

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:socialize_app/models/user.dart';
+import 'package:socialize_app/pages/home.dart';
+import 'package:socialize_app/widgets/progress.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -6,15 +13,136 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  TextEditingController searchController = TextEditingController();
+  Future<QuerySnapshot> searchResultFuture;
+
+  handleSearch(String query) {
+    Future<QuerySnapshot> users = usersRef
+        .where('displayName', isGreaterThanOrEqualTo: query)
+        .getDocuments();
+
+    setState(() {
+      searchResultFuture = users;
+    });
+  }
+
+  clearSearch() {
+    searchController.clear();
+  }
+
+  buildSearchField() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: TextFormField(
+        controller: searchController,
+        decoration: InputDecoration(
+          hintText: 'Search for a user...',
+          filled: true,
+          prefixIcon: Icon(
+            Icons.account_box,
+            size: 28.0,
+          ),
+          suffixIcon: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: clearSearch,
+          ),
+        ),
+        onFieldSubmitted: handleSearch,
+      ),
+    );
+  }
+
+  Container buildNoContent() {
+    final Orientation orientation = MediaQuery.of(context).orientation;
+    return Container(
+      child: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            SvgPicture.asset(
+              'images/search.svg',
+              height: orientation == Orientation.portrait ? 300.0 : 200.0,
+            ),
+            Text(
+              'Find Users',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w600,
+                fontSize: 60.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  buildSearchResults() {
+    return FutureBuilder(
+      future: searchResultFuture,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return circularProgress();
+        }
+        List<UserResult> searchResults = [];
+        snapshot.data.documents.forEach((doc) {
+          User user = User.fromDocument(doc);
+          UserResult searchResult = UserResult(user);
+          searchResults.add(searchResult);
+        });
+        return ListView(
+          children: searchResults,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).accentColor.withOpacity(0.6),
+      appBar: buildSearchField(),
+      body:
+          searchResultFuture == null ? buildNoContent() : buildSearchResults(),
+    );
   }
 }
 
 class UserResult extends StatelessWidget {
+  final User user;
+
+  UserResult(this.user);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Theme.of(context).primaryColor.withOpacity(0.6),
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () => print(''),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+                backgroundColor: Colors.grey,
+              ),
+              title: Text(
+                user.displayName,
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              subtitle:
+                  Text(user.username, style: TextStyle(color: Colors.white)),
+            ),
+          ),
+          Divider(
+            height: 2.0,
+            color: Colors.white54,
+          ),
+        ],
+      ),
+    );
   }
 }
