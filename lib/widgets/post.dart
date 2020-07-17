c7791754e61526e43bc18ca6dff11c981ea315bf
sabanansari <alisaban52@gmail.com>
2020-07-17 11:16:30 +0530

    onDeleteFollower created in node and delete post functionality added

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -107,6 +107,7 @@ class _PostState extends State<Post> {
           return circularProgress();
         }
         User user = User.fromDocument(snapshot.data);
+        bool isPostOwner = currentUserId == ownerId;
         return ListTile(
           leading: CircleAvatar(
             backgroundImage: CachedNetworkImageProvider(user.photoUrl),
@@ -121,14 +122,80 @@ class _PostState extends State<Post> {
             ),
           ),
           subtitle: Text(location),
-          trailing: IconButton(
-            onPressed: () => print('deleting post'),
+          trailing: isPostOwner?IconButton(
+            onPressed: () => handleDeletePost(context),
             icon: Icon(Icons.more_vert),
-          ),
+          ):Text(''),
         );
       },
     );
   }
+
+// Note: To delete post, ownerId and currentUserId must be equal, so they can be used interchangeabl
+  deletePost() async{
+//delete post itself
+postRef
+.document(ownerId)
+.collection('userPosts')
+.document(postId)
+.get().then((doc){
+  if(doc.exists){
+    doc.reference.delete()
+  }
+});
+
+//delete uploaded image for the post
+storageRef.child('post_$postId.jpg').delete();
+//then delete all activity feed notifications
+QuerySnapshot activityFeedSnapshot = await
+activityFeedRef
+.document(ownerId)
+.collection('feedItems')
+.where('postId', isEqualTo: postId)
+.getDocuments();
+
+activityFeedSnapshot.documents.forEach((doc) {
+  if(doc.exists){
+    doc.reference.delete();
+  }
+ });
+
+ //then delete all comments
+ QuerySnapshot commentsSnapshot = await
+ commentsRef.document(postId).collection('comments').getDocuments();
+
+commentsSnapshot.documents.forEach((doc) { 
+  if(doc.exists){
+    doc.reference.delete();
+  }
+});
+}
+
+  handleDeletePost(BuildContext parentContext){
+    return showDialog(
+      context: parentContext,
+      builder: (context){
+        return SimpleDialog(title: Text('Remove this post?'),
+        children: <Widget>[
+          SimpleDialogOption(
+             onPressed: (){
+               Navigator.pop(context);
+               deletePost();
+             },
+             child: Text('Delete',
+             style: TextStyle(
+               color: Colors.red
+             ),
+             ),
+          ),
+          SimpleDialogOption(
+            onPressed: ()=> Navigator.pop(context),
+            child: Text('Cancel'),)
+        ],);
+      });
+  }
+
+
 
   handleLikePost() {
     bool _isLiked = likes[currentUserId] == true;
